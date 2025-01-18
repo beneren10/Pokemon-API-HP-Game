6090e33c7c325f21d7b3fd2a67b77aefd5c1760d
Ben Eren <ben@bens-air.home>
2025-01-18 18:21:14 +0000

    winner stays on add

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const fruitLeft = document.querySelector('#listSection .new .left')
 const fruitright = document.querySelector('#listSection .new .right')
 const nextBtn = document.querySelector('body input')
 
+const scores = document.querySelectorAll('.container p span')
+
 document.addEventListener('DOMContentLoaded',randPokeId)
 document.addEventListener('click',next)
 
 let randomNumbers = [];
-for(let i = 0; i < 6; i++) {
+for(let i = 0; i < 152; i++) {
   randomNumbers.push(Math.floor(Math.random() * 151));
 }
 
@@ -17,29 +19,45 @@ let rightPokeName = ''
 let leftPokeHP = 0;
 let leftPokeName = ""
 
+
+let score = [0,0]
+
 function next(e){
     e.preventDefault()
+}
+
+async function randPokeId(){
+    leftPokeAPI(randomNumbers[0])
+    rightPokeAPI(randomNumbers[1])
 }
 
 function compare(input){
     if (!input){
         if (leftPokeHP > rightPokeHP){
             alert(`Correct ${leftPokeName} has ${leftPokeHP}HP and is higher than ${rightPokeName} with ${rightPokeHP}HP`)
+            rightPokeAPI(randomNumbers[Math.floor(Math.random()*151)])
         } else if (rightPokeHP > leftPokeHP) {
             alert(`incorrect ${rightPokeName} has ${rightPokeHP}HP and is lower than ${leftPokeName} with ${leftPokeHP}HP`)
+            leftPokeAPI(randomNumbers[Math.floor(Math.random()*151)])
         }
+        
+
     } else {
         if (leftPokeHP > rightPokeHP){
             alert(`Incorrect ${leftPokeName} has ${leftPokeHP}HP and is lower than ${rightPokeName} with ${rightPokeHP}HP`)
+            rightPokeAPI(randomNumbers[Math.floor(Math.random()*151)])
         } else if (rightPokeHP > leftPokeHP) {
             alert(`Correct ${rightPokeName} has ${rightPokeHP}HP and is higher than ${leftPokeName} with ${leftPokeHP}HP`)
+            leftPokeAPI(randomNumbers[Math.floor(Math.random()*151)])
         }
+        
     }
-}
-
-async function randPokeId(){
-    leftPokeAPI(randomNumbers[0])
-    rightPokeAPI(randomNumbers[1])
+    console.log(scores)
+    // scores.forEach(span => {
+    //     span[0].textContent = 'Games Played'
+    //     span[1].textContent = 'Score'
+    //     span[2].textContent = 'Win Percentage'
+    // })
 }
 
 async function leftPokeAPI(value){
@@ -53,7 +71,6 @@ async function leftPokeAPI(value){
         const pokeData = await response.json()
         if (typeof(value)=='number'){
             addLeft(pokeData)
-            console.log(pokeData)
         }
 
     }catch(error) {
@@ -62,18 +79,22 @@ async function leftPokeAPI(value){
 }
 
 function addLeft(param){
-    imgEle = document.createElement("img")
-    li = document.createElement('p')
+    if (document.querySelector('#listSection .new .left img')){
+        leftImgEle.remove()
+        leftLi.remove()
+    }
+    leftImgEle = document.createElement("img")
+    leftLi = document.createElement('p')
 
     leftPokeHP = param['stats'][0]['base_stat']
     leftPokeName = param['forms'][0].name
 
-    li.textContent = param['forms'][0].name + ' ID:' + param['id']
-    imgEle.style.height = '400px'
-    imgEle.src = param['sprites']['front_default']
+    leftLi.textContent = param['forms'][0].name + ' ID:' + param['id']
+    leftImgEle.style.height = '400px'
+    leftImgEle.src = param['sprites']['front_default']
     
-    fruitLeft.appendChild(imgEle)
-    fruitLeft.appendChild(li)
+    fruitLeft.appendChild(leftImgEle)
+    fruitLeft.appendChild(leftLi)
 }
 
 async function rightPokeAPI(value){
@@ -96,16 +117,22 @@ async function rightPokeAPI(value){
 }
 
 function addright(param){
-    imgEle = document.createElement("img")
-    li = document.createElement('p')
+    if (document.querySelector('#listSection .new .right img')){
+        console.log('hit')
+        rightImgEle.remove()
+        rightLi.remove()
+    }
+        rightImgEle = document.createElement("img")
+        rightLi = document.createElement('p')
     
-    rightPokeHP = param['stats'][0]['base_stat']
-    rightPokeName = param['forms'][0].name 
+        rightPokeHP = param['stats'][0]['base_stat']
+        rightPokeName = param['forms'][0].name 
 
-    li.textContent = param['forms'][0].name + ' ID:' + param['id']
-    imgEle.style.height = '400px'
-    imgEle.src = param['sprites']['front_default']
+        rightLi.textContent = param['forms'][0].name + ' ID:' + param['id']
+        rightImgEle.style.height = '400px'
+        rightImgEle.src = param['sprites']['front_default']
     
-    fruitright.appendChild(imgEle)
-    fruitright.appendChild(li)
+        fruitright.appendChild(rightImgEle)
+        fruitright.appendChild(rightLi)
+
 }
